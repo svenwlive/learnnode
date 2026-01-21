fb98ef8641faa379037f1224919aee6c5ae76277
Svenw <74722783+svenwlive@users.noreply.github.com>
2026-01-21 13:56:18 +0200

    make itemlist

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,34 @@
 <script setup>
 import { ref } from 'vue';
 
-    let message = ref('Hello Vue!');
+    let items = ref(['Sai', 'Piim', 'Viin']);
+    let newItem = ref('');
+function add() {
+    if(newItem.value.trim() != '') {
+        items.value.push(newItem.value.trim());
+    };
+    newItem.value = '';
+}
 </script>
 
 <template>
     <div class="container">
-        <h1>{{ message.split('').reverse().join('') }}</h1>
-        <button class="button is-primary" @click="message = 'CLICK!'">Click Me</button>
-        <input class="input" v-model="message">
+        <div class="field has-addons mt-2">
+            <div class="control is-expanded">
+                <input class="input" type="text" v-model="newItem" placeholder="Enter text..." @keypress.enter="add()">
+            </div>
+            <div class="control">
+                <button class="button is-info" @click="add()">
+                    Add item
+                </button>
+            </div>
+        </div>
+        <div class="content">
+            <h1>All Items</h1>
+            <ul>
+                <li v-for="item in items">{{ item }}</li>
+            </ul>
+        </div>
     </div>
 </template>
 
