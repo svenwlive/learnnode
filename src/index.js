abf5cfeb0382574e4acdca29f0117ab79c6709a8
Svenw <74722783+svenwlive@users.noreply.github.com>
2026-03-18 13:11:10 +0200

    Add XMLHttpRequest

--- src/index.js
+++ src/index.js
@@ -5,7 +5,8 @@ import { createMemoryHistory, createWebHashHistory, createWebHistory, createRout
 import ToDo from './pages/ToDo.vue';
 import Modals from './pages/Modals.vue';
 import Messages from './pages/Messages.vue';
-import Validation from './pages/Validation.vue'
+import Validation from './pages/Validation.vue';
+import ChuckNorris from './pages/ChuckNorris.vue';
 
 
 const routes = [
@@ -13,6 +14,7 @@ const routes = [
   { path: '/modals', component: Modals, name: 'Modals' },
   { path: '/messages', component: Messages, name: 'Messages' },
   { path: '/validation', component: Validation, name: 'Validation' },
+  { path: '/chuck', component: ChuckNorris, name: 'Chuck Norris' },
 ];
 
 export const router = createRouter({
---
+++ src/pages/ChuckNorris.vue
@@ -0,0 +1,21 @@
+<script setup>
+    import { ref } from 'vue';
+
+    let joke = ref('')
+    function reqListener() {
+        let data = JSON.parse(this.responseText);
+        console.log(data.value);
+        joke.value = data.value;
+    }
+
+    const req = new XMLHttpRequest();
+    req.addEventListener("load", reqListener);
+    req.open("GET", "https://api.chucknorris.io/jokes/random");
+    req.send();
+</script>
+
+<template>
+    <div class="container content">
+        <h1>{{ joke }}</h1>
+    </div>
+</template>
