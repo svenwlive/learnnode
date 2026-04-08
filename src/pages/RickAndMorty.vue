1c85d7c641a5b86921229443f9ddee0f9c2c1f58
Svenw <svenwlive@icloud.com>
2026-04-08 13:37:00 +0300

    Morty! Im bored! Im gonna kill you!

--- src/pages/RickAndMorty.vue
+++ src/pages/RickAndMorty.vue
@@ -13,6 +13,8 @@ let pagination = ref({
 
 let current = ref(1);
 
+let searchInput = ref('');
+
 // let res = await axios.get("https://rickandmortyapi.com/api/character");
 // characters.value = res.data.results;
 // pagination.value = res.data.info;
@@ -23,7 +25,8 @@ async function getCharacters(page) {
     current.value = page;
     let res = await axios.get('https://rickandmortyapi.com/api/character', {
         params: {
-            page: page
+            page: page,
+            name: searchInput.value
         }
     });
     characters.value = res.data.results;
@@ -58,13 +61,50 @@ let pages = computed(() => {
         pages[i] = i;
     }
 
-    return pages.filter(p => p);
+    return pages.filter((p) => p);
 });
 
+// debounce
+let searchTimeout = null;
+
+function search() {
+    clearTimeout(searchTimeout);
+    searchTimeout = setTimeout(async () => {
+        await getCharacters(1);
+    }, 500);
+};
 </script>
 <template>
     <div class="container">
+        <div class="field has-addons">
+            <div class="control is-expanded">
+                <input @input="search" v-model="searchInput" class="input" type="text" placeholder="Find a character">
+            </div>
+            <div class="control">
+                <button class="button is-info" @click="search">Search</button>
+            </div>
+        </div>
+
         <nav class="pagination is-centered" role="navigation" aria-label="pagination">
+            <button class="pagination-previous" :disabled="!pagination.prev" @click="prev">
+                Previous
+            </button>
+            <button class="pagination-next" :disabled="!pagination.next" @click="next">
+                Next page
+            </button>
+            <ul class="pagination-list">
+                <li v-for="page in pages">
+                    <span v-if="page === '...'" class="pagination-ellipsis">&hellip;</span>
+                    <button v-else-if="page === current" class="pagination-link is-current" aria-label="Page 46"
+                        aria-current="page">
+                        {{ page }}
+                    </button>
+                    <button v-else class="pagination-link" aria-label="Goto page 1" @click="getCharacters(page)">
+                        {{ page }}
+                    </button>
+                </li>
+
+        <!-- <nav class="pagination is-centered" role="navigation" aria-label="pagination">
             <button class="button" :disabled="!pagination.prev" @click="prev">Prev</button>
             <button class="button" @click="next" :disabled="!pagination.next">Next</button>
             <ul class="pagination-list">
@@ -72,9 +112,10 @@ let pages = computed(() => {
                     <span v-if="page === '...'" class="pagination-ellipsis">&hellip;</span>
                     <button v-else-if="page === current" class="pagination-link" aria-label="Page 46">{{ page }}</button>
                     <button v-else class="pagination-link" aria-label="Goto page 1" @click="getCharacters(page)">{{ page }}</button>
-                </li>
+                </li>-->
             </ul>
-        </nav>
+        </nav> 
+        
         <div class="columns is-multiline">
             <div class="column is-3" v-for="character in characters">
                 <CharacterCard :character="character" />
